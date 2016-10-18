00b85e3e91308376a9db08ef786e6a67ea5748ef
gilmoregrills <robinfarrowyonge@gmail.com>
2016-10-18 22:22:07 +0100

    make SentenceRuiner fully functional, comment out debug prints

--- lab03/SentenceRuiner.java
+++ lab03/SentenceRuiner.java
@@ -12,25 +12,35 @@ public class SentenceRuiner {
 		String sentence = input.nextLine();
 		char[] inputArray = sentence.toCharArray(); //can I condense this line and the above
 
-		System.out.println((char[])inputArray);
+		//System.out.println((char[])inputArray);
 
 		//reverse the array
+		//took some hella twisty thinking but I think this is the right way to do it. 
+		//I miss JavaScript's .reverse() method
+		
+		for (int j = 0; j < inputArray.length / 2; j++) {
+			char tmp = inputArray[j];
+			inputArray[j] = inputArray[inputArray.length - j - 1];
+			inputArray[inputArray.length - j - 1] = tmp;	
+		}
 
-		System.out.println((char[])inputArray);	
-	
+		//System.out.println((char[])inputArray);	
+		//loop through each char, and remove it if it's a space or punctuation. If it's a letter, change its case!
+		//current solution is kind of hacky, an empty/null char still takes up memory space.
+		//for a while toUpperCase wasn't working, because I was just calling toUpperCase(x). toUpperCase actually RETURNS the 
+		//upper case value, it doesn't change the value itself. 
+
 		for (int i = 0; i <= (inputArray.length - 1); i++) {
 			if (!Character.isLetter(inputArray[i])) {
 				char blank = '\u0000';	
 				inputArray[i] = blank;
 			} else if (Character.isUpperCase(inputArray[i])) {
-				Character.toLowerCase(inputArray[i]);
+				inputArray[i] = Character.toLowerCase(inputArray[i]);
 			} else {
-				Character.toUpperCase(inputArray[i]);
+				inputArray[i] = Character.toUpperCase(inputArray[i]);
 			}		
 		}		
-		//loop through each char, and remove it if it's a space or punctuation. If it's a letter, change its case!
-		//current solution is kind of hacky, an empty/null char still takes up memory space.
-		
+				
 		System.out.println((char[])inputArray);	
 	}
 
