9d0f55545ebdf4e6bdd27c62309f0c3f13fca2b2
Robin Yonge <robinfarrowyonge@gmail.com>
2016-10-31 09:15:48 +0000

    added third variant of ex08, using a for-each loop

--- lab02/ex08/Cubes3.java
+++ lab02/ex08/Cubes3.java
@@ -1,6 +1,7 @@
-public class Cubes2 {
+public class Cubes3 {
 	public static void main (String[] args) {
-		for (int i = 1; i < 10; i++)	{
+		int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		for (int i : numbers)	{
 			System.out.println("The cube of "+i+" is "+(i * i * i));
 		}
 	}
