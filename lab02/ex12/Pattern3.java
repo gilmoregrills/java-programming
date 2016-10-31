7cba07fad0be112e8e8b5bf3f66a497762d1db36
Robin Yonge <robinfarrowyonge@gmail.com>
2016-10-31 10:42:04 +0000

    add comments to Pattern3 and rightAligned

--- lab02/ex12/Pattern3.java
+++ lab02/ex12/Pattern3.java
@@ -3,6 +3,9 @@ public class Pattern3 {
 		int rows = Integer.parseInt(args[0]);
 		String output = "";
 		for (int i = 1; i <= rows; i++) {
+		//adds int i to the output String each loop then prints it
+		//first loop adds '1' to the empty string, prints '1' 
+		//second loop adds '2' to that string, and prints '12'
 			output += i;
 			System.out.println(output);
 		}
--- lab02/ex13/rightAligned.java
+++ lab02/ex13/rightAligned.java
@@ -1,11 +1,15 @@
 public class rightAligned {
      public static void main (String[] args) {
           int rows = Integer.parseInt(args[0]);
-          int j = 0;
-          int k = 0;
-
-          for (int i = 0; i <= rows; i++) {
-               for (j = rows; j > k; j--) {
+	  int j = 0;
+	  int k = 0;
+	  //if I could change the j>k evaluation in the first loop
+	  //then I wouldn't need to declare k in advance. As it is
+	  //it seems ugly to only declare k up here and j in the 
+	  //loop so here they both are 
+          for (int i = 1; i <= rows; i++) {
+          
+	       for (j = rows; j > k; j--) {
                     System.out.print(" ");
                }
                for (k = 0; k < i; k++) {
