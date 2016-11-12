fd95365b602b9f1777e5d2793ca7c2d531d37810
gilmoregrills <robinfarrowyonge@gmail.com>
2016-11-12 20:30:54 +0000

    add ArrayLists solution for ex28, change ex28 pt 1 to a for each loop

--- lab05/LetterGrades.java
+++ lab05/LetterGrades.java
@@ -2,8 +2,8 @@ public class LetterGrades {
 	public static void main(String[] args) {
 		char gradeBand = 'U';
 		
-		for (int i = 0; i <= args.length - 1; i++) {
-			int tmp = Integer.parseInt(args[i]);;	
+		for (String i : args) {
+			int tmp = Integer.parseInt(i);;	
 			if (tmp >= 50 && tmp < 55) {
 				gradeBand = 'D';
 			} else if (tmp >= 55 && tmp < 60) {
@@ -15,7 +15,7 @@ public class LetterGrades {
 			} else if (tmp < 50) {
 				gradeBand = 'U';
 			}
-			System.out.println("Student "+i+" score is: "+args[i]+" and grade is: "+gradeBand);
+			System.out.println("Student "+i+" score is: "+tmp+" and grade is: "+gradeBand);
 		}
 	}
 }
--- lab05/LetterGrades1.java
+++ lab05/LetterGrades1.java
@@ -1,5 +1,26 @@
-public class LetterGrades {
-	public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class LetterGrades1 {
+	public static void main(String[] args) {
+		char gradeBand = 'U';	
+		ArrayList<String> stringList = new ArrayList(Arrays.asList(args));
+
+		for (String i : stringList) {
+		int tmp = Integer.parseInt(i);	
+			if (tmp >= 50 && tmp < 55) {
+				gradeBand = 'D';
+			} else if (tmp >= 55 && tmp < 60) {
+			       gradeBand = 'C';	
+			} else if (tmp >= 60 && tmp < 70) {
+				gradeBand = 'B';
+			} else if (tmp >= 70) {
+				gradeBand = 'A';
+			} else if (tmp < 50) {
+				gradeBand = 'U';
+			}
+			System.out.println("Student "+i+" score is: "+tmp+" and grade is: "+gradeBand);
+
+		}
 	}
 }
