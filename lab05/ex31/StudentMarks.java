8e7656ee0541ae0b229968c72171470a90a20036
gilmoregrills <robinfarrowyonge@gmail.com>
2016-11-12 21:58:15 +0000

    start work on ex31

---
+++ lab05/ex31/StudentMarks.java
@@ -0,0 +1,17 @@
+public class StudentMarks {
+	
+	double[] marks = new double[7];
+
+	public double getMark(int i) {
+		return marks[i];
+	}
+
+	public void setMark(int i, double d) {
+		marks[i] = d;
+	}
+
+	
+
+	public static void main(String[] args) {
+	}
+}
