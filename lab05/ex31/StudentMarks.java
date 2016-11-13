ceb86b2a1aa4fbbf5813b90b35b091045df54bbe
gilmoregrills <robinfarrowyonge@gmail.com>
2016-11-13 10:20:03 +0000

    add solutions to ex31 along with a test class

--- lab05/ex31/StudentMarks.java
+++ lab05/ex31/StudentMarks.java
@@ -1,17 +1,44 @@
+/**
+ * A class that holds an array of student's
+ * marks.
+ *
+ * @author Robin Yonge
+ * @date 13/10/16
+ */
 public class StudentMarks {
 	
-	double[] marks = new double[7];
-
+	double[] marks = new double[7]; //instance variable
+	/**
+	* accessor method that returns a specified mark.
+	*/
 	public double getMark(int i) {
 		return marks[i];
 	}
-
+	/**
+	 * mutator method that logs a mark to the array.
+	 */
 	public void setMark(int i, double d) {
 		marks[i] = d;
 	}
+	/**
+	 * method returns the mean value of the student's marks.
+	 */
+	public double meanMark() {
+		double mean = 0;
+		for (double d : marks) {
+			mean += d;
+		}
+		return (mean / (marks.length));
+	}
 
-	
-
-	public static void main(String[] args) {
+	/**
+	 *  method returns the highest of the student's 7 marks. 
+	 */
+	public double highestMark() {
+		double tmp = 0;
+		for (double d : marks) {
+			tmp = (d > tmp) ? tmp = d : tmp;
+		}
+		return tmp;
 	}
 }
---
+++ lab05/ex31/StudentMarksMain.java
@@ -0,0 +1,26 @@
+/**
+ * A test class for StudentMarks.java.
+ *
+ * @author Robin Yonge
+ * @date 13/10/16
+ */
+public class StudentMarksMain {
+	public static void main(String[] args) {
+	
+	StudentMarks testStudent = new StudentMarks();
+
+	testStudent.setMark(0, 56);
+	testStudent.setMark(1, 76);
+	testStudent.setMark(2, 87);
+	testStudent.setMark(3, 72);
+	testStudent.setMark(4, 45);
+	testStudent.setMark(5, 99);
+	testStudent.setMark(6, 87);
+
+	System.out.println("This student's mark for their third assignment is: "+testStudent.getMark(3));
+	System.out.println("This student's overall average mark is: "+testStudent.meanMark());
+	System.out.println("This student's highest mark is: "+testStudent.highestMark());
+
+	
+	}
+}
