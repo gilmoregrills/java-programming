a0e171f76a70bc6dec7e20658f42fea437a5b077
gilmoregrills <robinfarrowyonge@gmail.com>
2016-11-24 16:41:08 +0000

    fix exception handling in ex39, solve problem

--- lab07/ex39/AverageInt.java
+++ lab07/ex39/AverageInt.java
@@ -7,33 +7,34 @@ public class AverageInt {
 	Scanner input = new Scanner(System.in);
 
 
-	System.out.println("How many numbers would you like to test?");
+	System.out.println("How many numbers would you like to input??");
 	int n = input.nextInt();
 	int[] integers = new int[n];
 	System.out.println("Please enter "+n+" integers");
 	int tmp = 0;
 		//try to check if j < 0 and if it's not throw error and catch
 		//it with the 'must be positive' error message and reprompt
-	for (int i = 0; i < n; i++) {
-		int j = 0;
-		try {
-			j = input.nextInt();
-			if (j < 0) {
+		for (int i = 0; i < n; i++) {
+			int j = 0;
+			try {
+				j = input.nextInt();
+				if (j < 0) {
 				throw new InputException();
-			}
+				}
+			} catch(InputMismatchException e) {
+				System.out.println("Please enter an actual Integer");
+				input.next();
+				i--;
+				continue;
+			} catch (InputException e) {
+				System.out.println("Please enter a POSITIVE Integer");
+				i--;
+				continue;
+			}	
+			integers[i] = j;
+			tmp += j;
+
 		}
-		catch(InputMismatchException|InputException e) {
-			System.out.println("Please enter a positive Integer");
-			input.next();
-			i--;
-			continue;
-		}
-		integers[i] = j;
-		tmp += j;
-		//current problem: if an exception occurs, overwrites
-		//one index on the array
-	}
-	
 	System.out.println("The average of all your integers is: "+(tmp / n));	
 	}
 }
