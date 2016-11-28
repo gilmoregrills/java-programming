8c4229e636e7f7d70325249d3a64df5c0e02b0c3
Robin Yonge <robinfarrowyonge@gmail.com>
2016-11-28 10:21:46 +0000

    add solution for lab07a > ex01

---
+++ lab07a - Generics/ex01/Reverser.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Reverser {
+	public static void main(String[] args) {
+		String[] strArray = {"Pardon", "me", "are", "you", "Aaron", "Burr", "sir?"};
+		Integer[] intArray = {1, 3, 5, 7, 9};
+
+		reverse(intArray);
+		reverse(strArray);
+
+		System.out.println(Arrays.toString(intArray)+"\n"+Arrays.toString(strArray));	
+	}
+
+	public static <E> void reverse(E[] inputArray) {
+		for (int i = 0; i < inputArray.length / 2; i++) {
+			E tmp = inputArray[i];
+			inputArray[i] = inputArray[inputArray.length - i -1];
+			inputArray[inputArray.length - i - 1] = tmp;
+		}
+	}
+}
