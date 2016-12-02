3c3621f826ce78f5e39f50e3353edfc03c99fe0c
gilmoregrills <robinfarrowyonge@gmail.com>
2016-12-02 15:50:11 +0000

    add solutions for lab08a ex01

--- lab08a - GUIs/ex01/RGButtons.java
+++ lab08a - GUIs/ex01/RGButtons.java
@@ -1,34 +1,60 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class RGButtons extends JFrame {
 
-	private JPanel mainPanel;
+	/**
+	 * Instantiating all my components
+	 */
 	private JButton buttonRed;
 	private JButton buttonBlue;
 	private JButton buttonGreen;
-
-	public RGButtons() {
-		this.setTitle("Red, Green, or Blue??");
-		this.setSize(500, 200);
-		
+	private JPanel colorField;
+	private JPanel contentPane;
+	
+	/**
+	 * This method creates the UI elements,
+	 * labels them, and adds behaviour
+	 */
+	private void initializeUI() {
+		colorField = new JPanel(); 
+		colorField.setBackground(Color.BLACK);
 		buttonRed = new JButton("Red");
+		buttonRed.addActionListener((ActionEvent click) -> {
+			colorField.setBackground(Color.RED);
+		});
 		buttonBlue = new JButton("Blue");
+		buttonBlue.addActionListener((ActionEvent click) -> {
+			colorField.setBackground(Color.BLUE);
+		});
 		buttonGreen = new JButton("Green");
+		buttonGreen.addActionListener((ActionEvent click) -> {
+			colorField.setBackground(Color.GREEN);
+		});
 		
-		mainPanel = new JPanel();
-		mainPanel.add(buttonRed);
-		mainPanel.add(buttonBlue);
-		mainPanel.add(buttonGreen);
-
-		Container cont = this.getContentPane();
-		cont.add(mainPanel, BorderLayout.NORTH);
-		cont.add(buttonRed, BorderLayout.WEST);
-		cont.add(buttonBlue, BorderLayout.CENTER);
-		cont.add(buttonGreen, BorderLayout.EAST);
-		this.pack();
-		this.setVisible(true);
+		createLayout();	
+		setTitle("Red, Green, or Blue??");
+		setSize(220, 100);
+		setVisible(true);
 	}
+	/**
+	 * This method creates the layout
+	 * manager, creates the main panel
+	 * and adds everything to it
+	 */
+	private void createLayout() {
+		GridLayout layout = new GridLayout(2, 2, 5, 5);
+		contentPane = new JPanel(layout);
+		contentPane.add(buttonRed);
+		contentPane.add(buttonBlue);
+		contentPane.add(buttonGreen);
+		contentPane.add(colorField);
+		this.setContentPane(contentPane);
+	}
+	public RGButtons() {
+		initializeUI();
+		}
 
 	public static void main(String[] args) {
 		RGButtons rgb = new RGButtons();
