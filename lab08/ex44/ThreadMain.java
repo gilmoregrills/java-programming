4be33e0b077ef31381b53568edd44a2d832b427f
gilmoregrills <robinfarrowyonge@gmail.com>
2016-12-10 23:42:35 +0000

    add additional sanity checks to be sure it's working

--- lab08/ex44/ThreadMain.java
+++ lab08/ex44/ThreadMain.java
@@ -1,16 +1,25 @@
 public class ThreadMain {
 	public static void main(String[] args) {
-		FirstTask ft1 = new FirstTask("Thread A", 50); 
-		FirstTask ft2 = new FirstTask("Thread B", 50);
+		FirstTask ft1 = new FirstTask("Thread A ", 50); 
+		FirstTask ft2 = new FirstTask("Thread B ", 50);
 		SecondTask st1 = new SecondTask(50);
 
 		Thread t1 = new Thread(ft1);
 		Thread t2 = new Thread(ft2);
 		Thread t3 = new Thread(st1);
+//sanity checks, after anxiety caused by multiple identical outputs
+		System.out.println(t1.getState());
+		System.out.println(t2.getState());
+		System.out.println(t3.getState());
 
 		t1.start();
 		t2.start();
 		t3.start();
+//again, sanity checks, may print in the middle of threads, if 
+//threads have not all terminated, doesn't matter tbh
+		System.out.println(t1.getState());
+		System.out.println(t2.getState());
+		System.out.println(t3.getState());
 
 	}
 }
