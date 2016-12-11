721d60f24c128bd10765559d37ecc640cb76b060
gilmoregrills <robinfarrowyonge@gmail.com>
2016-12-11 21:17:46 +0000

    swapped <E> to <T> on reverser generic method

--- lab07a - Generics/ex01/Reverser.java
+++ lab07a - Generics/ex01/Reverser.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Reverser {
 	public static void main(String[] args) {
 		String[] strArray = {"Pardon", "me", "are", "you", "Aaron", "Burr", "sir?"};
-		Integer[] intArray = {1, 3, 5, 7, 9};
+		Integer[] intArray = {1, 3, 5, 7, 9, 10};
 
 		reverse(intArray);
 		reverse(strArray);
 
 		System.out.println(Arrays.toString(intArray)+"\n"+Arrays.toString(strArray));	
 	}
-//	maybe E should be T for type? 
-	public static <E> void reverse(E[] inputArray) {
+//	switched out <E> for <T> 
+	public static <T> void reverse(T[] inputArray) {
 		for (int i = 0; i < inputArray.length / 2; i++) {
-			E tmp = inputArray[i];
+			T tmp = inputArray[i];
 			inputArray[i] = inputArray[inputArray.length - i -1];
 			inputArray[inputArray.length - i - 1] = tmp;
 		}
